c4b56e446ac26ebe32dc09372a2119e9cd182c95
Michael Barney, Jr <mbarneyme@gmail.com>
2020-03-01 15:11:00 -0500

    Fix for 0.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 import setuptools
-import pypandoc
-
-long_description = pypandoc.convert_file('README.md', 'rst')
 
 setuptools.setup(
     name="localvariables-demo-giphy",
@@ -9,7 +6,7 @@ setuptools.setup(
     author="Michael Barney",
     author_email="mbarneyjr5309@gmail.com",
     description="A demo for localvariables",
-    long_description=long_description,
+    long_description='long_description',
     packages=setuptools.find_packages(),
     install_requires=['giphy_client'],
     entry_points={
